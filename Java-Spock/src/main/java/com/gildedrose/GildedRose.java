e964c455c01f78ff10a1cddf3711fcdc3029b870
Avdbogaert <alexis.vandenbogaert@oniryx.be>
2024-04-03 22:20:34 +0200

    refactor update quality and add spock tests

--- Java-Spock/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Spock/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,13 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+
 class GildedRose {
+
+    private final int MAXIMUM_QUALITY = 50;
+    private final int MINIMAL_QUALITY = 0;
+    private final int DEFAULT_QUALITY_MODIFIER = -1;
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -8,55 +15,47 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+        Arrays.stream(items).forEach(item -> {
+            item.sellIn--;
+            int modifier = DEFAULT_QUALITY_MODIFIER;
+            if (item.name.contains("Aged Brie")) {
+                modifier = Math.abs(modifier); // set modifier to positive
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (item.name.contains("Backstage passes")) {
+                modifier = backStagePassesValueModifier(item); // set to specific valye
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+            if (item.name.contains("Conjured")) { // multiplies modifier by two when conjured
+                modifier *= 2;
             }
+
+            if (item.name.contains("Sulfuras")) {
+                return; // We stop before quality value modification
+            }
+
+            updateQuality(item, modifier);
+
+        });
+    }
+
+    private void updateQuality(Item item, int modifier) {
+        int value = item.quality + (item.sellIn > 0 ? modifier : modifier * 2);
+        item.quality = value <= MINIMAL_QUALITY ? MINIMAL_QUALITY : Math.min(value, MAXIMUM_QUALITY);
+
+    }
+
+    private int backStagePassesValueModifier(Item item) {
+        if (item.sellIn < 0) { // concert is over
+            return Integer.MIN_VALUE;
+        } else if (item.sellIn <= 5) { // 5 days or less left
+            return 3;
+        } else if (item.sellIn <= 10) { // ten days or less left
+            return 2;
+        } else {
+            return -DEFAULT_QUALITY_MODIFIER; // give the inverse value of the Default value
         }
     }
+
 }
--- Java-Spock/src/test/groovy/com/gildedrose/GildedRoseSpec.groovy
+++ Java-Spock/src/test/groovy/com/gildedrose/GildedRoseSpec.groovy
@@ -7,19 +7,102 @@ import spock.lang.Specification
  */
 class GildedRoseSpec extends Specification {
 
-    def "should update quality correctly"() {
+    def "should update quality of aged brie"() {
 
-        given: "some items"
-        Item[] items = [new Item("foo", 0, 0)];
-
-        and: "the application with these items"
-        GildedRose app = new GildedRose(items);
+        given: "the application"
+        GildedRose app = new GildedRose(list as Item[])
 
         when: "updating quality"
-        app.updateQuality();
+        app.updateQuality()
 
         then: "the quality is correct"
-        app.items[0].name == "fixme"
+        app.items[0].sellIn == sellIn
+        app.items[0].quality == quality
+
+        where:
+        list                            | sellIn | quality
+        [new Item("Aged Brie", 0, 0)]   | -1     | 2
+        [new Item("Aged Brie", 5, 10)]  | 4      | 11
+        [new Item("Aged Brie", 10, 50)] | 9      | 50
     }
+
+    def "should update quality of Back stages passes"() {
+
+        given: "the application"
+        GildedRose app = new GildedRose(list as Item[])
+
+        when: "updating quality"
+        app.updateQuality()
+
+        then: "the quality is correct"
+        app.items[0].sellIn == sellIn
+        app.items[0].quality == quality
+
+        where:
+        list                                                 | sellIn | quality
+        [new Item("Backstage passes michel sardou", 0, 0)]   | -1     | 0
+        [new Item("Backstage passes michel sardou", 6, 10)]  | 5      | 13
+        [new Item("Backstage passes michel sardou", 11, 10)] | 10     | 12
+        [new Item("Backstage passes michel sardou", 13, 10)] | 12     | 11
+    }
+
+    def "should update quality of conjured"() {
+
+        given: "the application"
+        GildedRose app = new GildedRose(list as Item[])
+
+        when: "updating quality"
+        app.updateQuality()
+
+        then: "the quality is correct"
+        app.items[0].sellIn == sellIn
+        app.items[0].quality == quality
+
+        where:
+        list                               | sellIn | quality
+        [new Item("Conjured Brie", 0, 0)]  | -1     | 0
+        [new Item("Conjured Brie", 5, 10)] | 4      | 8
+        [new Item("Conjured Brie", 0, 50)] | -1     | 46
+    }
+
+    def "should update quality of composite items"() {
+
+        given: "the application"
+        GildedRose app = new GildedRose(list as Item[])
+
+        when: "updating quality"
+        app.updateQuality()
+
+        then: "the quality is correct"
+        app.items[0].sellIn == sellIn
+        app.items[0].quality == quality
+
+        where:
+        list                                                      | sellIn | quality
+        [new Item("Conjured Brie", 0, 10)]                        | -1     | 6
+        [new Item("Conjured Brie", 5, 10)]                        | 4      | 8
+        [new Item("Conjured Aged Brie", 5, 10)]                   | 4      | 12
+        [new Item("Backstage passes Conjured Aged Brie", 10, 10)] | 9      | 14
+    }
+
+    def "should never update quality of Sulfuras"() {
+
+        given: "the application"
+        GildedRose app = new GildedRose(list as Item[])
+
+        when: "updating quality"
+        app.updateQuality()
+
+        then: "the quality is correct"
+        app.items[0].sellIn == sellIn
+        app.items[0].quality == quality
+
+        where:
+        list                                    | sellIn | quality
+        [new Item("Sulfuras Aged Brie", 0, 80)] | -1     | 80
+        [new Item("Sulfuras", 5, 80)]           | 4      | 80
+        [new Item("Conjured Sulfuras", 10, 80)] | 9      | 80
+    }
+
 
 }
